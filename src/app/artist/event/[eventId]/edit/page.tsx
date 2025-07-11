817b8c2d3feb7dcf369adf40cb3806520345bf99
John Kennady_official <j.johny.111@gmail.com>
2025-07-11 12:41:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/artist/event/[eventId]/edit/page.tsx
+++ src/app/artist/event/[eventId]/edit/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import EventForm from '@/app/artist/event/form/page';
-export default function EditEventPage({ params }: { params: { eventId: string } }) {
-    return <EventForm eventId={params.eventId} />;
+
+export default function EditEventPage({ params: { eventId } }: { params: { eventId: string } }) {
+    return <EventForm eventId={eventId} />;
 }
